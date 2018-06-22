afeeaec59d6047515353c97246f2ed76a4eac42e
Jabran Rafique <hello@jabran.me>
2018-06-22 07:23:21 +0100

    update html structure

--- theme/footer.php
+++ theme/footer.php
@@ -11,18 +11,18 @@
 
 ?>
 
-	</div><?php // #content ?>
+            </div><?php // #content ?>
 
-</div><?php // #page ?>
-<footer id="colophon" class="row prs-footer">
-    <div class="col-sm-12 prs-footer__info">
-        &copy; 2017&mdash;<?php echo date('Y'); ?> Ruby Idrees
-        <span class="sep"> &bull; </span>
-        All rights reserved.
-    </div><?php // .site-info ?>
-</footer><?php // #colophon ?>
+        </div><?php // #page ?>
+        <footer id="colophon" class="row prs-footer">
+            <div class="col-sm-12 prs-footer__info">
+                &copy; 2017&mdash;<?php echo date('Y'); ?> Ruby Idrees
+                <span class="sep"> &bull; </span>
+                All rights reserved.
+            </div><?php // .site-info ?>
+        </footer><?php // #colophon ?>
 
-<?php wp_footer(); ?>
-
+        <?php wp_footer(); ?>
+    </div>
 </body>
 </html>
